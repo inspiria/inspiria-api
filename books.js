1f188cf01529fa8b904eef4d71af37db666ea63c
Tadas Razmislavicius <tadas.razmislavicius@gmail.com>
2020-02-16 20:30:06 +0200

    Returns books covers from api.

--- books.js
+++ books.js
@@ -14,14 +14,14 @@ exports.createList = async function createList() {
 
   con.connect((err) => { if (err) { console.log('Error connecting to DB'); } });
 
-  const rows = await query('SELECT book_id, title, subtitle, published_year, cover_image_lg FROM books');
+  const rows = await query('SELECT book_id, title, subtitle, published_year, cover_image_md FROM books');
   const books = rows.map(row => {
     let book = {}
     book["id"] = row.book_id;
     book["title"] = row.title;
     book["subtitle"] = row.subtitle;
     book["year"] = row.published_year;
-    book["coverImage"] = row.cover_image_lg;
+    book["coverImageUrl"] = "https://edtechbooks.org/book_cover_images/" + row.cover_image_md;
     return book;
   });
   // con.end();
