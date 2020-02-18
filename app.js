c307eb9b991bbd69c88d14c621dcbb386e5989d5
Tadas Razmislavicius <tadas.razmislavicius@gmail.com>
2020-02-18 11:50:10 +0200

    Compress book to zip file

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
-const express = require('express')
+const express = require('express');
+const path = require('path');
 const books = require('./booksList');
 const book = require('./book');
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
 app.get('/', async function (req, res) {
   res.send('OK');
@@ -17,8 +18,20 @@ app.get('/books', async function (req, res) {
 app.get('/book/:bookId', async function (req, res) {
   try {
     const id = req.params.bookId;
-    const result = await book.get(id);
-    return res.send(result);
+    const bookPath = await book.get(id);
+    let filePath = path.join(__dirname, bookPath);
+    return res.sendFile(filePath);
+  } catch(e) {
+    return res.status(404).send(e);
+  }
+})
+
+//for testing purposes only
+app.get('/book-json/:bookId', async function (req, res) {
+  try {
+    const id = req.params.bookId;
+    const result = await book.jsonBook(id, false);
+    return res.json(result);
   } catch(e) {
     return res.status(404).send(e);
   }
--- book.js
+++ book.js
@@ -1,22 +1,23 @@
 const mysql = require('mysql');
 const util = require('util');
+const admZip = require('adm-zip');
+const fs = require('fs');
+const { promisify } = require('util');
+
 
 const con = mysql.createConnection({
   host: 'localhost',
   user: 'root',
   password: 'root1234',
-  database: 'books'
+  database: 'books_full'
 });
 
-exports.get = async function get(bookId) {
-
-  console.log(`book/${bookId}`);
-
+exports.jsonBook = async function jsonBook(bookId, full = true) {
   const query = util.promisify(con.query).bind(con);
 
   con.connect((err) => { if (err) { console.log('Error connecting to DB'); } });
 
-  const rows = await query(`SELECT chapter_id, book_id, title, language_id, short_name, order_number, text, text_processed, citation, section_headers FROM chapters WHERE book_id = ${bookId};`);
+  const rows = await query(`SELECT chapter_id, book_id, title, language_id, short_name, order_number, text, text_processed, citation, section_header, show_number, section_headers FROM chapters WHERE book_id = ${bookId};`);
 
   if (rows.length == 0) {
     throw "Book not found";
@@ -30,12 +31,46 @@ exports.get = async function get(bookId) {
     chapter["language"] = row.language_id;
     chapter["shortName"] = row.short_name;
     chapter["order"] = row.order_number;
-    chapter["text"] = row.text;
-    chapter["textProcessed"] = row.text_processed;
     chapter["citation"] = row.citation;
+    chapter["sectionHeader"] = row.section_header;
+    chapter["showNumber"] = row.show_number;
+    chapter["text"] = full ? row.text : "Text";
+    chapter["textProcessed"] = full ? row.text_processed : "Text processed";
     chapter["sectionHeaders"] = row.section_headers;
     return chapter;
   });
-  // con.end();
+
+  chapters.sort(function(a, b) {
+    if (a.order < b.order) { return -1; }
+    if (a.order > b.order) { return 1; }
+
+    if (a.short_name < b.short_name) { return -1; }
+    if (a.short_name > b.short_name) { return 1; }
+    return 0;
+  });
   return chapters;
+}
+
+async function exportBook(book, path) {
+  let data = JSON.stringify(book);
+  let filePath = `${path}/book.json`;
+  let zipPath = `${path}/book.zip`;
+  
+  fs.mkdirSync(path, { recursive: true });
+  fs.writeFileSync(filePath, data);
+
+  var zip = new admZip();
+  zip.addLocalFile(filePath)
+  let writeZip = promisify(zip.writeZip).bind(zip)
+  await writeZip(zipPath);
+
+  return zipPath;
+}
+
+exports.get = async function get(bookId) {
+  fs.mkdirSync(`./tmp/`, { recursive: true });
+  let path = `.tmp/${bookId}`;
+  let book = await this.jsonBook(bookId);
+  let result = await exportBook(book, path);
+  return result;
 }
