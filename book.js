be1a6257f6844cd28fed8202330b4a6d190ee808
Tadas Razmislavicius <tadas.razmislavicius@gmail.com>
2020-03-31 11:41:27 +0300

    Update naming of images

--- book.js
+++ book.js
@@ -19,15 +19,15 @@ exports.jsonBook = async function jsonBook(bookId, full = true) {
   const list = await books.get(bookId);
   const info = list[0];
   const author = await authors.getAuthors(bookId);
-  
+
   author.forEach(auth => { auth.pictureName = auth.pictureName !== null ? `${bookId}_author_${auth.pictureName}` : null });
-  
+
   //add author pictures
   var imagesToDownload = author
-  .map(auth => {
-    return auth.pictureName != null ? { "name": auth.pictureName, "url": auth.pictureUrl } : null;
-  })
-  .filter(el => el != null );
+    .map(auth => {
+      return auth.pictureName != null ? { "name": auth.pictureName, "url": auth.pictureUrl } : null;
+    })
+    .filter(el => el != null);
 
   //        `SELECT chapter_id, book_id, short_name, title, order_number, section_header, show_number, show_pdf, last_updated, copyright_override, published, synchronize, IF(pw="",0,1) AS pw_exists, IF(editor_notes="",0,1) AS editor_notes_exist  FROM chapters WHERE book_id = ? '`
   const q = `SELECT chapter_id, book_id, short_name, title, order_number, section_header, show_number, show_headings, last_updated, copyright_override, published, synchronize, parent_id, language_id, text, citation, section_headers
@@ -46,34 +46,34 @@ exports.jsonBook = async function jsonBook(bookId, full = true) {
   book["chapters"] = await Promise.all(chaptersRows.map(async row => {
     let chapter = {}
     let imagesAndText = await sanitizeText(row.text, bookId);
-    let text =  imagesAndText.text;
+    let text = imagesAndText.text;
 
     //add images from chapters
     imagesToDownload = imagesToDownload.concat(imagesAndText.images)
-    
+
     var show_headings = row.show_headings;
     if (row.section_header == 1) { sectionHeaderStarted = true; }
     if (row.section_header != 1 && sectionHeaderStarted) { show_headings = 1 }
 
-    chapter["id"]                = row.chapter_id;
-    chapter["bookId"]            = row.book_id;
-    chapter["shortName"]         = row.short_name;
-    chapter["title"]             = row.title;
-    chapter["orderNumber"]       = row.order_number;
-    chapter["isSectionHeader"]   = Boolean(row.section_header);
-    chapter["showNumber"]        = Boolean(row.show_number);
-    chapter["showHeadings"]      = Boolean(show_headings);
-    chapter["lastUpdated"]       = row.last_updated;
-    chapter["parentId"]          = row.synchronize == 1 ? row.parent_id : null;
+    chapter["id"] = row.chapter_id;
+    chapter["bookId"] = row.book_id;
+    chapter["shortName"] = row.short_name;
+    chapter["title"] = row.title;
+    chapter["orderNumber"] = row.order_number;
+    chapter["isSectionHeader"] = Boolean(row.section_header);
+    chapter["showNumber"] = Boolean(row.show_number);
+    chapter["showHeadings"] = Boolean(show_headings);
+    chapter["lastUpdated"] = row.last_updated;
+    chapter["parentId"] = row.synchronize == 1 ? row.parent_id : null;
     chapter["copyrightOverride"] = row.copyright_override === "" ? null : row.copyright_override;
-    chapter["language"]          = row.language_id;
+    chapter["language"] = row.language_id;
     chapter["citation"]          = full ? row.citation        : (row.citation        == null ? null : "citation goes here");
     chapter["text"]              = full ? text                : (text                == null ? null : "text goes here");
     chapter["sectionHeaders"]    = full ? row.section_headers : (row.section_headers == null ? null : "section_headers goes here");
 
     return chapter;
   }));
-  
+
   if (full) {
     //filter images
     imagesToDownload = imagesToDownload.filter((obj, pos, arr) => {
@@ -91,12 +91,12 @@ exports.jsonBook = async function jsonBook(bookId, full = true) {
 async function sanitizeText(text, bookId) {
   var images = [];
   const sanitizeOptions = {
-    allowedTags: sanitizeHtml.defaults.allowedTags.concat([ 'img' ]),
+    allowedTags: sanitizeHtml.defaults.allowedTags.concat(['img']),
     transformTags: {
-      'img': function(tagName, attribs) {
+      'img': function (tagName, attribs) {
         let url = attribs.src;
-        var name = url.split('/').pop();
-        images.push({"name":name, "url":url})
+        var name =  new URL(url).pathname.split('/').pop();
+        images.push({ "name": name, "url": url })
         return {
           tagName: tagName,
           attribs: { src: name }
@@ -111,7 +111,7 @@ async function sanitizeText(text, bookId) {
 
 async function downloadImage(uri, filename) {
   const requestSync = util.promisify(request);
-  let result = await requestSync(uri, {encoding: 'binary'});
+  let result = await requestSync(uri, { encoding: 'binary' });
   if (result.statusCode == 200) {
     fs.writeFileSync(filename, result.body, 'binary');
   }
@@ -155,10 +155,10 @@ exports.getBookStatus = function getBookStatus(bookId) {
   try {
     let info = fs.statSync(path)
     return {
-      "size": filesize(info.size, {round: 0}),
+      "size": filesize(info.size, { round: 0 }),
       "updated": info.mtime.toLocaleString(),
-    } 
+    }
   } catch (e) {
-    return { "error: " : e.toString() }
+    return { "error: ": e.toString() }
   }
 }
